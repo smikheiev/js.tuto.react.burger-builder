201cc94027ccbe9c6708cad3c8aca9e00d74c3fd
Serhii Mikheiev <mikh.s.o@gmail.com>
2018-06-07 20:42:31 +0200

    add prop type validation

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
-const Burger = (props) => {
+const burger = (props) => {
   const types = ['bread-bottom', 'bread-top', 'meat', 'cheese', 'salad', 'bacon',]
   return types.map((type, index) => <BurgerIngredient key={index} type={type}/>)
 }
 
-export default Burger
+export default burger
--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,46 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 import Aux from '../../../hoc/Aux'
 import styles from './BurgerIngredient.css'
 
-const BurgerIngredient = (props) => {
-  let className = ''
-  let children = null
+class BurgerIngredient extends Component {
+  render () {
+    let className = ''
+    let children = null
 
-  switch (props.type) {
-    case 'bread-bottom':
-      className = styles.BreadBottom
-      break
-    case 'bread-top':
-      className = styles.BreadTop
-      children = (
-        <Aux>
-          <div className={styles.Seeds1}></div>
-          <div className={styles.Seeds2}></div>
-        </Aux>
-      )
-      break
-    case 'meat':
-      className = styles.Meat
-      break
-    case 'cheese':
-      className = styles.Cheese
-      break
-    case 'salad':
-      className = styles.Salad
-      break
-    case 'bacon':
-      className = styles.Bacon
-      break
-    default:
-      return null
+    switch (this.props.type) {
+      case 'bread-bottom':
+        className = styles.BreadBottom
+        break
+      case 'bread-top':
+        className = styles.BreadTop
+        children = (
+          <Aux>
+            <div className={styles.Seeds1}></div>
+            <div className={styles.Seeds2}></div>
+          </Aux>
+        )
+        break
+      case 'meat':
+        className = styles.Meat
+        break
+      case 'cheese':
+        className = styles.Cheese
+        break
+      case 'salad':
+        className = styles.Salad
+        break
+      case 'bacon':
+        className = styles.Bacon
+        break
+      default:
+        return null
+    }
+
+    return (
+      <div className={className}>
+        {children}
+      </div>
+    )
   }
+}
 
-  return (
-    <div className={className}>
-      {children}
-    </div>
-  )
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired
 }
 
 export default BurgerIngredient
