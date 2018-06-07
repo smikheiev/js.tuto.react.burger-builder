71324bef8f2951ebe85bcb8ce826344f661d544e
Serhii Mikheiev <mikh.s.o@gmail.com>
2018-06-07 20:51:38 +0200

    add burger style

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+import styles from './Burger.css'
 
 const burger = (props) => {
-  const types = ['bread-bottom', 'bread-top', 'meat', 'cheese', 'salad', 'bacon',]
-  return types.map((type, index) => <BurgerIngredient key={index} type={type}/>)
+  const types = ['bread-top', 'salad', 'cheese', 'bacon', 'meat', 'bread-bottom']
+  const ingredients = types.map((type, index) => <BurgerIngredient key={index} type={type}/>)
+  return (
+    <div className={styles.Burger}>
+      {ingredients}
+    </div>
+  )
 }
 
 export default burger
