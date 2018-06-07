b6c33ac471966a6d220b4e9b587708e9081df1f7
Serhii Mikheiev <mikh.s.o@gmail.com>
2018-06-07 21:27:03 +0200

    render ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 import styles from './Burger.css'
 
 const burger = (props) => {
-  const types = ['bread-top', 'salad', 'cheese', 'bacon', 'meat', 'bread-bottom']
-  const ingredients = types.map((type, index) => <BurgerIngredient key={index} type={type}/>)
+  const ingredientTypes = Object.keys(props.ingredients)
+  const allIngredientTypes = ingredientTypes.map((type) => {
+    const ingredientCount = props.ingredients[type]
+    return Array(ingredientCount).fill(type)
+  })
+  const allIngredientTypesFlattened = allIngredientTypes.reduce((previousValue, currentValue) => {
+    return [...previousValue, ...currentValue]
+  })
+  const ingredientComponents = allIngredientTypesFlattened.map((type, index) => {
+    return <BurgerIngredient key={index} type={type}/>
+  })
+
   return (
     <div className={styles.Burger}>
-      {ingredients}
+      {ingredientComponents}
     </div>
   )
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,11 +4,20 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 2,
+      bacon: 1,
+      cheese: 2,
+      meat: 2,
+    }
+  }
+
   render () {
     return (
       <Aux>
         <div>
-          <Burger/>
+          <Burger ingredients={this.state.ingredients}/>
         </div>
         <div>Build controls</div>
       </Aux>
