ead05038b96f6779e1571aa8d27fa1bdeda63493
Serhii Mikheiev <mikh.s.o@gmail.com>
2018-06-09 19:00:51 +0200

    update modal component only when show property changed

--- src/components/Ui/Modal/Modal.js
+++ src/components/Ui/Modal/Modal.js
@@ -1,23 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import styles from './Modal.css'
 import Aux from '../../../hoc/Aux'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
-  <Aux>
-    <Backdrop
-      show={props.show}
-      click={props.closeModal}/>
-    <div
-      className={styles.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}>
-      {props.children}
-    </div>
-  </Aux>
-)
+class modal extends Component {
+  shouldComponentUpdate (nextProps, nextState) {
+    return nextProps.show !== this.props.show
+  }
+
+  render () {
+    return (
+      <Aux>
+        <Backdrop
+          show={this.props.show}
+          click={this.props.closeModal}/>
+        <div
+          className={styles.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </Aux>
+    )
+  }
+}
 
 export default modal
