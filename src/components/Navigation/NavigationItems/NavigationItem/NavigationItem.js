fbd26b646ab3c34235c492c6cbb8f6a2652990b9
Serhii Mikheiev <mikh.s.o@gmail.com>
2018-06-09 17:52:37 +0200

    base navigation

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import styles from './NavigationItem.css'
+
+const navigationItem = (props) => (
+  <li className={styles.NavigationItem}>
+    <a
+      className={props.active ? styles.active : null}
+      href={props.link}
+    >{props.children}</a>
+  </li>
+)
+
+export default navigationItem
