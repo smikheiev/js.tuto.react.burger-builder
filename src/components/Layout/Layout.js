e0f84fe7c7ff3ec8ac15dfac401f85dd8b179ca5
Serhii Mikheiev <mikh.s.o@gmail.com>
2018-06-09 17:29:26 +0200

    base toolbar component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import Aux from '../../hoc/Aux'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 import styles from './Layout.css'
 
 const layout = (props) => (
   <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar/>
     <main className={styles.Content}>
       {props.children}
     </main>
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from './Toolbar.css'
+
+const toolbar = (props) => (
+  <header className={styles.Toolbar}>
+    <div>Menu</div>
+    <div>Logo</div>
+    <nav>
+      ...
+    </nav>
+  </header>
+)
+
+export default toolbar
