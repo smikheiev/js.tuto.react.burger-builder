2687fb8c40aa44db1da6300e7441520df49ae207
Serhii Mikheiev <mikh.s.o@gmail.com>
2018-06-10 22:26:37 +0200

    base orders; fixing navigation items

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 import styles from './NavigationItem.css'
 
 const navigationItem = (props) => (
   <li className={styles.NavigationItem}>
-    <a
-      className={props.active ? styles.active : null}
-      href={props.link}
-    >{props.children}</a>
+    <NavLink
+      activeClassName={styles.active}
+      exact={props.exact}
+      to={props.link}
+    >{props.children}</NavLink>
   </li>
 )
 
---
+++ src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import styles from './Order.css'
+
+const order = (props) => (
+  <div className={styles.Order}>
+    <p>Ingredients: Salda(1)</p>
+    <p>Price: <strong>USD 5,45</strong></p>
+  </div>
+)
+
+export default order
